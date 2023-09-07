a98195185183467fc8ed727601c256244e081a14
Albert Snow <ajsnow2012@gmail.com>
2023-09-06 19:18:04 -0500

    remove meta files

--- src/main/java/com/example/demostatemachine/model/data/repositories/Meta.java
+++
@@ -1,9 +0,0 @@
-package com.example.demostatemachine.model.data.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Meta extends CrudRepository<com.example.demostatemachine.model.data.entities.Meta, Long> {
-	//	List<com.example.demostatemachine.model.data.entities.Movie> findById(long id);
-	//	List<com.example.demostatemachine.model.data.entities.Movie> findByTitle(String title);
-	//	List<com.example.demostatemachine.model.data.entities.Movie> findByReleaseYear(int release_year);
-}
