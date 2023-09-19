ea0741a7d9e850f9a30f21b46a08545a460917f8
Albert Snow <ajsnow2012@gmail.com>
2023-09-19 00:13:45 -0500

    adding yaml api stuff

---
+++ src/main/java/com/example/demostatemachine/config/API.java
@@ -0,0 +1,14 @@
+package com.example.demostatemachine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class API {
+//	@Bean
+//	public Docket API() {
+//		return new Docket(DocumentationType.SWAGGER_3)
+//	}
+}
